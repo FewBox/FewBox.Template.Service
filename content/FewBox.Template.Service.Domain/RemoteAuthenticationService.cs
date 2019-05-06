8bba45aef3d1640b5d99be947f1f9a58dbd637b1
Peter Pang <mv@live.cn>
2019-05-06 18:20:18 +0800

    No DB & Private.

--- content/FewBox.Template.Service.Domain/RemoteAuthenticationService.cs
+++ content/FewBox.Template.Service.Domain/RemoteAuthenticationService.cs
@@ -1,14 +1,32 @@
 using System;
 using System.Collections.Generic;
+using FewBox.Core.Utility.Net;
+using FewBox.Core.Web.Dto;
 using FewBox.Core.Web.Security;
+using FewBox.Template.Service.Model.Configs;
+using Microsoft.AspNetCore.Http;
 
 namespace FewBox.Template.Service.Domain
 {
     public class RemoteAuthenticationService : IAuthenticationService
     {
+        private IHttpContextAccessor HttpContextAccessor { get; set; }
+        private SecurityConfig SecurityConfig { get; set; }
+
+        public RemoteAuthenticationService(IHttpContextAccessor httpContextAccessor, SecurityConfig securityConfig)
+        {
+            this.HttpContextAccessor = httpContextAccessor;
+            this.SecurityConfig = securityConfig;
+        }
         public IList<string> FindRolesByControllerAndAction(string controller, string action)
         {
-             throw new NotImplementedException();
+            var headers = new List<Header>();
+            foreach(var header in this.HttpContextAccessor.HttpContext.Request.Headers)
+            {
+                headers.Add(new Header { Key = header.Key, Value=header.Value });
+            }
+            var response = RestfulUtility.Get<PayloadResponseDto<IList<string>>>($"{this.SecurityConfig.Protocol}://{this.SecurityConfig.Host}:{this.SecurityConfig.Port}/api/security/{controller}/{action}", headers);
+            return response.Payload;
         }
 
         public IList<string> FindRolesByMethod(string method)
@@ -18,12 +36,7 @@ namespace FewBox.Template.Service.Domain
 
         public bool IsValid(string username, string password, string userType, out object userId, out IList<string> roles)
         {
-            bool isValid = false;
-            Guid id = Guid.Empty;
-            roles = null;
-            // Todo: Add validation here.
-            userId = id;
-            return isValid;
+            throw new NotImplementedException();
         }
     }
 }
---
+++ content/FewBox.Template.Service.Model/Configs/SecurityConfig.cs
@@ -0,0 +1,9 @@
+namespace FewBox.Template.Service.Model.Configs
+{
+    public class SecurityConfig
+    {
+        public string Protocol { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
